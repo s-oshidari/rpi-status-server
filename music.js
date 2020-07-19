5b1cf847dbf0220fbd5196ffe795d87c21994f30
oshiemon <shinobiashisan@gmail.com>
2020-07-19 16:05:06 +0900

    add karada dandan

--- music.js
+++ music.js
@@ -1,11 +1,13 @@
+const song_karada_dandan = require('./songs/karada_dandan');
 const song_station = require('./songs/station');
 const song_oni_no_pants_1 = require('./songs/oni_no_pants_1');
 const song_oni_no_pants_2 = require('./songs/oni_no_pants_2');
 
 const playlist = {
-  station: song_station,
-  oni_no_pants_1: song_oni_no_pants_1,
-  oni_no_pants_2: song_oni_no_pants_2,
+  karada_dandan: song_karada_dandan,
+  // station: song_station,
+  // oni_no_pants_1: song_oni_no_pants_1,
+  // oni_no_pants_2: song_oni_no_pants_2,
 };
 
 module.exports = class Music {
---
+++ songs/karada_dandan.js
@@ -0,0 +1,42 @@
+module.exports = {
+  baseLength: 120,
+  melody: [
+    { scale: 're', level: 1, length: 1 },
+    { scale: 're', level: 1, length: 1 },
+    { scale: 'fa#', level: 1, length: 2 },
+    { scale: 'ra', level: 1, length: 1 },
+    { scale: 're', level: 2, length: 2 },
+    { scale: 'si', level: 1, length: 5 },
+    { scale: 'do#', level: 2, length: 2 },
+    { scale: 'do#', level: 2, length: 2 },
+
+    { scale: 're', level: 1, length: 1 },
+    { scale: 're', level: 1, length: 1 },
+    { scale: 'fa#', level: 1, length: 2 },
+    { scale: 'ra', level: 1, length: 1 },
+    { scale: 're', level: 2, length: 2 },
+    { scale: 'si', level: 1, length: 5 },
+    { scale: 'do#', level: 2, length: 2 },
+    { scale: 'do#', level: 2, length: 2 },
+
+    { scale: 're', level: 1, length: 2 },
+    { scale: 'fa#', level: 1, length: 1 },
+    { scale: 'fa#', level: 1, length: 1 },
+    { scale: 'ra', level: 1, length: 1 },
+    { scale: 're', level: 2, length: 2 },
+    { scale: 'si', level: 1, length: 5 },
+    { scale: 'do#', level: 2, length: 2 },
+    { scale: 'do#', level: 2, length: 2 },
+
+    { scale: 'ra#', level: 1, length: 1 },
+    { scale: 'ra#', level: 1, length: 1 },
+    { scale: 'ra#', level: 1, length: 6 },
+
+    { scale: 'do', level: 2, length: 1 },
+    { scale: 'do', level: 2, length: 7 },
+
+    { scale: 're', level: 2, length: 1 },
+    { scale: 're', level: 2, length: 1 },
+    { scale: 're', level: 2, length: 8 },
+  ],
+};
